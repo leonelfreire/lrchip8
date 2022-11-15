e88d94c3eab2d605dc05be2bc8655f2172171693
Leonel Freire <leonelfreire@gmail.com>
2022-11-15 20:16:47 -0300

    Fix the order in which the vF register is written.

--- src/chip8.rs
+++ src/chip8.rs
@@ -309,8 +309,10 @@ impl Chip8 {
     // Set Vx = Vx SHR 1.
     // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
     fn op_8xy6(&mut self, x: usize) {
-        self.v[0xF] = self.v[x] & 1;
+        let vf = self.v[x] & 1;
+
         self.v[x] >>= 1;
+        self.v[0xF] = vf;
     }
 
     // 8xy7 - SUBN Vx, Vy
@@ -326,8 +328,10 @@ impl Chip8 {
     // Set Vx = Vx SHL 1.
     // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
     fn op_8xye(&mut self, x: usize) {
-        self.v[0xF] = (self.v[x] >> 7) & 1;
+        let vf = (self.v[x] >> 7) & 1;
+
         self.v[x] <<= 1;
+        self.v[0xF] = vf;
     }
 
     // 9xy0 - SNE Vx, Vy
