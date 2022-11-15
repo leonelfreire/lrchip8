ae7b9b786616740db9b092583d35053377271497
Leonel Freire <leonelfreire@gmail.com>
2022-11-15 11:33:11 -0300

    Fix timers.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use lrchip8::{
 };
 use sdl2::pixels::Color;
 
+const FPS: f64 = 60.0;
+const SECS_PER_FRAME: f64 = 1.0 / FPS;
 const SCALE_FACTOR: usize = 24;
 
 fn main() {
@@ -34,7 +36,7 @@ fn main() {
         chip8.video_rows(),
         SCALE_FACTOR,
         Color::BLACK,
-        Color::GREY,
+        Color::RGB(225, 225, 225),
     );
 
     let mut input = Input::init(event_pump);
@@ -43,10 +45,14 @@ fn main() {
 
     chip8.load(&rom);
 
+    let secs_per_frame = Duration::from_secs_f64(SECS_PER_FRAME);
+
     'mainloop: loop {
         let start_time = Instant::now();
 
-        for _ in 0..32 {
+        chip8.update_timers();
+
+        for _ in 0..12 { // 720hz
             let keys = input.read();
 
             if keys[input::KEY_QUIT] {
@@ -59,11 +65,8 @@ fn main() {
 
         video.draw(chip8.read_video());
 
-        chip8.update_timers();
-
-        let elapsed_time = Instant::now() - start_time;
-        thread::sleep(Duration::from_secs_f64(
-            (1.0 / 60.0) - elapsed_time.as_secs_f64(),
-        ));
+        thread::sleep(
+            secs_per_frame.saturating_sub(Instant::now().saturating_duration_since(start_time)),
+        );
     }
 }
